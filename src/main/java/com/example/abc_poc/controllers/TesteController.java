fa06b7f2a6ca51f26e5437e6b0fa1dab85ca3562
Francisco Nascimento <francisco.nascimento@bradesco.com.br>
2025-08-18 11:20:52 -0300

    Adicionando gson dependency

--- src/main/java/com/example/abc_poc/controllers/TesteController.java
+++ src/main/java/com/example/abc_poc/controllers/TesteController.java
@@ -28,9 +28,7 @@ public class TesteController implements ApiRest {
         );
     }
 
-    /**
-     * Endpoint to create a new computador
-     */
+
     @PostMapping("/computadores")
     public void createComputadores(@RequestBody Map<String, Object> computador) {
         createComputador(computador);
